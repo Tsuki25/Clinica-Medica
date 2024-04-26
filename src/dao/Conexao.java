4c140adfd46feb62113c1f0ca047cf8080e2f6a9
Eduardo Ferreira <StringDova@users.noreply.github.com>
2024-04-26 10:47:49 -0300

    criação_dao
    Criação do pacote Dao, da classe de conexão com o banco e o Dao do paciente.

---
+++ src/dao/Conexao.java
@@ -0,0 +1,25 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    Connection conn = null;
+
+    public Conexao() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinica", "root", "ifsp");
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão com o banco!");
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+}
